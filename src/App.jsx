28a764d06b548bed48be7d6f4d98c83e45026de2
code-ninja-labs <codeninjalabs@tutamail.com>
2025-02-13 16:17:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import ProtectedRoute from "./components/ProtectedRoute";
+import LandingPage from "./pages/landingpage/LandingPage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -10,14 +13,19 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Public routes */}
+        <Route path="/landing" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        
-        {/* Protected route requires authentication */}
-        <Route element={<ProtectedRoute />}>
+        <Route path="/mail-confirmed" element={<MailConfirmed />} />
+
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute redirectPath="/landing" />}>
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />}
-           <Route path="/mail-confirmed" element={<MailConfirmed />} /> 
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
+
+        {/* Redirect all other routes to landing */}
+        <Route path="*" element={<LandingPage />} />
       </Routes>
     </Router>
   );
