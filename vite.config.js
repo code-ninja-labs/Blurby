485f992bd1abffb5040312891e95639b5132de73
code-ninja-labs <codeninjalabs@tutamail.com>
2025-02-13 16:00:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['utils'],
-      output: {
-        globals: {
-          utils: 'utils', // Use the global name "utils"
-        },
-      },
+  resolve: {
+    alias: {
+      '@': '/src', // Allows importing files like "@/pages/Dashboard"
     },
   },
 });
